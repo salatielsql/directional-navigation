9320214a91f190d339809489284477b0a13944aa
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2022-12-25 11:34:21 -0300

    test: setup jest and testing library

---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  ...require("@qsalatiel/prettier-config"),
+};
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+  testEnvironment: "jsdom",
+};
